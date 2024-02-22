7e2ee45383b8d8a178d3b939eeef3cf36d11f962
JsilvaM7 <159531285+JsilvaM7@users.noreply.github.com>
2024-02-22 21:29:54 +0000

    Add files via upload

--- Assign03Question4.java
+++ Assign03Question4.java
@@ -1,6 +1,6 @@
 public class Assign03Question4 {
     public static void main(String[] args) {
-        // Symbol 1
+        // Pattern 1
         System.out.println("1)");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 10; j++) {
@@ -9,7 +9,13 @@ public class Assign03Question4 {
             System.out.println();
         }
         
-        // Symbol 2
+        
+        
+        
+        
+        
+        
+        // Pattern 2
         System.out.println("\n2)");
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
@@ -18,19 +24,27 @@ public class Assign03Question4 {
             System.out.println();
         }
         
-        // Symbol 3
+        // Pattern 3
         System.out.println("\n3)");
-        for (int i = 5; i >= 1; i--) {
-            for (int j = 1; j < i; j++) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int k = 5; k >= i; k--) {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();
         }
         
-        // Symbol 4
+        
+        
+        
+        
+        
+        
+        
+        
+        // Pattern 4
         System.out.println("\n4)");
         for (int i = 1; i <= 5; i++) {
             for (int j = 5; j > i; j--) {
@@ -42,7 +56,7 @@ public class Assign03Question4 {
             System.out.println();
         }
         
-        // Symbol 5
+        // Pattern 5
         System.out.println("\n5)");
         for (int i = 1; i <= 5; i++) {
             for (int j = 5; j > i; j--) {
@@ -54,7 +68,15 @@ public class Assign03Question4 {
             System.out.println();
         }
         
-        // Symbol 6
+        
+        
+        
+        
+        
+        
+        
+        
+        // Pattern 6
         System.out.println("\n6)");
         for (int i = 1; i <= 5; i++) {
             for (int j = 5; j > i; j--) {
